9f54ceecf95c70f0796a81be8c22a2787de868ae
Philip <trumpet7347@gmail.com>
2016-10-05 13:06:23 -0500

    renaming to match project

--- src/AuctionHouseBrowser/Startup.cs
+++ src/AuctionHouseBrowser/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace AngularTest
+namespace AuctionHouseBrowser
 {
     public class Startup
     {
